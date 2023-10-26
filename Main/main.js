76b0a8777ea6a50b366820ddcb71b0c3e753619c
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2023-10-26 10:12:07 +0100

    render function completed

--- Main/main.js
+++ Main/main.js
@@ -1,6 +1,39 @@
 let expanded = document.getElementById("add-expanded");
 
+const apiEndpoint = "http://localhost:7777/topics'";
+
 function expand() {
   expanded.classList.toggle("add-topic-expanded-open");
 }
 
+//variable for revision list
+const revisionList = document.querySelector(".revision-list")
+//
+async function refreshTopics() {
+  const response = await fetch(apiEndpoint);
+  const data = await response.json();
+  revisionList.innerHTML = "";
+
+  for (const topics of data.payload) {
+    renderTopic(topics);
+  }
+}
+//
+function renderTopic(topics) {
+  const li = document.createElement("li");
+  const topicTitle = document.createElement("h3");
+  topicTitle.textContent = topics.topic;
+  topicTitle.className = "ListTitle";
+
+  const topicContent = document.createElement("p");
+  topicContent.textContent = topics.content;
+  topicContent.className = "ListContent";
+
+
+  const topicDate = document.createElement("p");
+  topicDate.textContent = topics.added_date;
+  topicDate.className = "ListDate";
+
+  li.append(topicTitle, topicContent, topicDate);
+  revisionList.appendChild(li);
+}
